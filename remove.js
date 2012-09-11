bd61102b00143009fbea81f22f17bd0ef2ec9bdd
Nick Baugh <niftylettuce@gmail.com>
2012-09-11 00:15:08 -0400

    fixed links

--- remove.js
+++ remove.js
@@ -1,13 +1,13 @@
 
-//     Block Quora Popup
+//     Block Quora Login Popup
 //     Copyright (c) 2012 Nick Baugh <niftylettuce@gmail.com>
 //     MIT Licensed
 
 // Automatically block the Quora login popup.
-// <https://github.com/niftylettuce/block-quora-popup>
+// <https://github.com/niftylettuce/block-quora-login-popup>
 
 // * Author: [@niftylettuce](https://twitter.com/#!/niftylettuce)
-// * Source: <https://github.com/niftylettuce/block-quora-popup>
+// * Source: <https://github.com/niftylettuce/block-quora-login-popup>
 
 // # remove
 
