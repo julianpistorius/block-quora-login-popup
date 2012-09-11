4e4852cb02efe2f117a8efbdfd94a907c96f1369
Nick Baugh <niftylettuce@gmail.com>
2012-09-10 22:33:13 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+
+# Block Quora Login Popup <sup>0.0.1</sup>
+
+Automatically block the Quora login popup.
+
+## Contributors
+
+* Nick Baugh <niftylettuce@gmail.com>
+
+## License
+
+MIT Licensed
---
+++ remove.js
@@ -0,0 +1,28 @@
+
+//     Block Quora Popup
+//     Copyright (c) 2012 Nick Baugh <niftylettuce@gmail.com>
+//     MIT Licensed
+
+// Automatically block the Quora login popup.
+// <https://github.com/niftylettuce/block-quora-popup>
+
+// * Author: [@niftylettuce](https://twitter.com/#!/niftylettuce)
+// * Source: <https://github.com/niftylettuce/block-quora-popup>
+
+// # remove
+
+;(function($) {
+
+  remove();
+  setInterval(remove, 5000);
+
+  function remove() {
+    // could have searched for _fb prefix (or) simply 'div' -- but we want to prevent it always :)
+    $('*').filter(fixed).remove();
+  }
+
+  function fixed() {
+    return $(this).css('position') === 'fixed';
+  }
+
+})(jQuery);
